5ad8f5ba8dcb17454a4a291ee343c7ffeaac51f9
Md Rabiul Hasan <rhr2416@gmail.com>
2025-03-16 12:48:48 +0600

    contact us page desing

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,5 +1,56 @@
-export const Contact = () =>{
- return <>
- <h1>This is Contact page</h1>
- </>
-}
+export const Contact = () => {
+  return (
+    <>
+      <div className="contact">
+        <div className="container contact-container">
+
+  <form className="colorful-form">
+    <h1 style={{textAlign:"center"}}>Contact With Us</h1>
+    <div className="form-group">
+      <label className="form-label" htmlFor="name">
+        Name:
+      </label>
+      <input
+        required=""
+        placeholder="Enter your name"
+        className="form-input"
+        type="text"
+      />
+    </div>
+    <div className="form-group">
+      <label className="form-label" htmlFor="email">
+        Email:
+      </label>
+      <input
+        required=""
+        placeholder="Enter your email"
+        className="form-input"
+        name="email"
+        id="email"
+        type="email"
+      />
+    </div>
+    <div className="form-group">
+      <label className="form-label" htmlFor="message">
+        Message:
+      </label>
+      <textarea
+        required=""
+        placeholder="Enter your message"
+        className="form-input"
+        name="message"
+        id="message"
+        defaultValue={""}
+      />
+    </div>
+    <button className="form-button" type="submit">
+      Submit
+    </button>
+  </form>
+
+
+        </div>
+      </div>
+    </>
+  );
+};
